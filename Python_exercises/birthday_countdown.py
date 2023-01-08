de40bb0c20bf5a0566ff6084ca547c4c8e5c233f
Michee_Tokatoka <tokatokakiyombo@gmail.com>
2023-01-08 20:54:47 +0200

    birthday_countdown base program

---
+++ Python_exercises/birthday_countdown.py
@@ -0,0 +1,20 @@
+import os
+import datetime
+os.system("cls")#clean terminal
+
+
+def birthday_countdown(age, birthmonth, birthday):
+    today = datetime.datetime.now()
+    birthday = datetime.datetime(today.year, birthmonth, birthday)
+
+    if birthday < today:
+        birthday = birthday.replace(year = today.year + 1)
+    return (birthday - today).days
+    
+age = 30
+birthmonth = 8
+birthday = 2
+
+days_until_birthday = birthday_countdown(age, birthmonth, birthday)
+
+print(f"There are {days_until_birthday} days until birthday")

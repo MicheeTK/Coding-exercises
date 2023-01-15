566bb0be033185fd885f56e7aa5a1c819b59b674
Michee_Tokatoka <tokatokakiyombo@gmail.com>
2023-01-15 11:05:24 +0200

    added html and js files

--- Tutorials/JS-DOM/DOM-selecting-element/DOM-selecting-element.js
+++ Tutorials/JS-DOM/DOM-selecting-element/DOM-selecting-element.js
@@ -1,1 +1,9 @@
-console.log("Michee");
+const mainHeading = document.getElementById("main-heading");
+console.log(mainHeading);
+
+const movieList = document.getElementsByClassName("movie-list");
+console.log(movieList[0]);
+
+const movies = document.getElementsByTagName("li");
+// const movieChoice = movies.slice(3);
+console.log(Object.values(movies)[0]);

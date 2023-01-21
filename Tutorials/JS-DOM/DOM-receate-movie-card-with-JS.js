12f20f3aa044d983026062b495d4ecc8eb48a39e
Michee_Tokatoka <tokatokakiyombo@gmail.com>
2023-01-21 23:04:23 +0200

    Added  DOM-receate-movie-card-with-JS file

---
+++ Tutorials/JS-DOM/DOM-receate-movie-card-with-JS.js
@@ -0,0 +1,69 @@
+/*Recreating the favourite movie card using JS ONLY*/
+
+//Create Elements
+const body = document.querySelector("body");
+const favMovieContainer2 = document.createElement("div.fav-movie-container-2");
+const mainHeading2 = document.createElement("h1");
+mainHeading2.setAttribute("id", "main-heading-2"); //Id attribute can not be added in one line of code
+
+//beginning of block of code that allows to create an element with properties in one line of code
+document.__proto__.customCreateElement = function (tag, attributes) {
+  var e = document.createElement(tag);
+
+  for (var a in attributes) e.setAttribute(a, attributes[a]);
+
+  return e;
+};
+//end of block of code that allows to create an element with properties in one line of code
+
+//Create Elements using the custom method
+const movieList2 = document.customCreateElement("ul", { id: "movie-list-2" });
+const movie1 = document.customCreateElement("li", { id: "movie-1" });
+const movie2 = document.customCreateElement("li", { id: "movie-2" });
+const movie3 = document.customCreateElement("li", { id: "movie-3" });
+const movie4 = document.customCreateElement("li", { id: "movie-4" });
+const movie5 = document.customCreateElement("li", { id: "movie-5" });
+
+//Populate created elements
+body.append(favMovieContainer2);
+favMovieContainer2.append(mainHeading2);
+favMovieContainer2.append(movieList2);
+movieList2.append(movie1);
+movieList2.append(movie2);
+movieList2.append(movie3);
+movieList2.append(movie4);
+movieList2.append(movie5);
+
+//Add and style content
+favMovieContainer2.style.width = "360px";
+favMovieContainer2.style.height = "200px";
+favMovieContainer2.style.margin = "100px auto";
+favMovieContainer2.style.textAlign = "center";
+favMovieContainer2.style.display = "flex";
+favMovieContainer2.style.flexDirection = "column";
+favMovieContainer2.style.border = "2px solid whitesmoke";
+favMovieContainer2.style.borderRadius = "10px";
+
+mainHeading2.textContent += "Favourite Movie Franchise--JS";
+mainHeading2.style.color = "#fff";
+mainHeading2.style.marginBlock = "10px";
+mainHeading2.style.fontSize = "22px";
+
+movie1.textContent += "The Matrix";
+movie2.textContent += "Star Wars";
+movie3.textContent += "Harry Potter";
+movie4.textContent += "Lord of the Rings";
+movie5.textContent += "Marvel";
+
+const allMoviesStyling = document.querySelectorAll("#movie-list-2 li");
+for (let textContent of Object.values(allMoviesStyling)) {
+  textContent.style.listStyleType = "none";
+  textContent.style.backgroundColor = "whitesmoke";
+  textContent.style.borderBottom = "1px solid black";
+  textContent.style.marginInline = "15px";
+  textContent.style.height = "26px";
+  textContent.style.display = "flex";
+  textContent.style.justifyContent = "center";
+  textContent.style.alignItems = "center";
+  textContent.style.fontWeight = "500";
+}
